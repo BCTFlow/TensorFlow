b99d4974cbd55d0e60ec18baf0ee96747eb23296
daneography <acena.dane@gmail.com>
2018-03-01 10:40:57 -0500

    Revert

--- cnn_mnist.py
+++ cnn_mnist.py
@@ -21,7 +21,7 @@ def cnn_model_fn(features, labels, mode):
       kernel_size=[5, 5],
       padding="same",
       activation=tf.nn.relu)
-      #pool2 = tf.layers.max_pooling2d(inputs=conv2, pool_size=[2, 2], strides=2)
+  pool2 = tf.layers.max_pooling2d(inputs=conv2, pool_size=[2, 2], strides=2)
 
   # Dense Layer
   pool2_flat = tf.reshape(pool2, [-1, 7 * 7 * 64])
